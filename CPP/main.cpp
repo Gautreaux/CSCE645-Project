564b6a6a30b159241aa7f6c39ba819aba4711f36
Spencer Gautreaux <37717452+Gautreaux@users.noreply.github.com>
2021-10-28 10:30:50 -0500

    adding inital CPP build setup

---
+++ CPP/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "raster.hpp"
+
+int main(void){
+    std::cout << "Hello world" << std::endl;
+}
---
+++ CPP/raster.hpp
@@ -0,0 +1,11 @@
+
+
+class Raster{
+private:
+    short width_exact;
+    short width_bytes;
+    short height;
+    short resolution;
+public:
+
+};
